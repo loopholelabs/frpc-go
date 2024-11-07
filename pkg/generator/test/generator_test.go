03eadf833bbf20677d19555e80cd296e2983fdb4
Luiz Aoqui <lgfa29@gmail.com>
2024-11-07 11:29:46 -0300

    Prevent deadlock on connection close and expose connection to the stream context (#52)
    During an RPC, the generated method blocks until a response is received
    or the passed context is cancelled, but if the connection is closed
    while waiting for either event, the method remains blocked.
    
    The new generated code also listens for a connection close event,
    unblocking the method.
    
    Signed-off-by: Luiz Aoqui <luizaoqui@loopholelabs.io>

--- pkg/generator/test/generator_test.go
+++ pkg/generator/test/generator_test.go
@@ -4,11 +4,18 @@ package test
 
 import (
 	"context"
+	"fmt"
 	"io"
+	"net/http"
+	"net/http/httptest"
+	"os"
 	"testing"
+	"time"
 
+	"github.com/loopholelabs/polyglot/v2"
 	"github.com/loopholelabs/testing/conn/pair"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestRPC(t *testing.T) {
@@ -129,4 +136,52 @@ func testClientStreaming(client *Client, t *testing.T) {
 	res, err := stream.CloseAndRecv()
 	assert.NoError(t, err)
 	assert.Equal(t, "Hello World", res.Message)
+}
+
+func TestRPCInvalidConnection(t *testing.T) {
+	// Create non-Frisbee server the client can connect to but not exchange
+	// messages, so the connection will be broken soon after connect.
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "test")
+	}))
+	t.Cleanup(ts.Close)
+
+	// Create a client and connect to test server.
+	client, err := NewClient(nil, nil)
+	require.NoError(t, err)
+
+	err = client.Connect(ts.Listener.Addr().String())
+	require.NoError(t, err)
+
+	// Make RPC request with a 3s timeout.
+	timeout := 3 * time.Second
+	if os.Getenv("CI") != "" {
+		timeout = 10 * time.Second
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), timeout)
+	t.Cleanup(cancel)
+
+	req := &Request{
+		Message: "Hello World",
+		Corpus:  RequestUNIVERSAL,
+	}
+	response, err := client.EchoService.Echo(ctx, req)
+
+	// Verify request doesn't block forever.
+	require.NoError(t, ctx.Err())
+
+	// Verify request fails.
+	require.Error(t, err)
+	require.Nil(t, response)
+}
+
+func TestEncodeDecodePreservesNilFields(t *testing.T) {
+	r := &Response{Message: "test", Test: nil}
+	b := polyglot.NewBuffer()
+	r.Encode(b)
+
+	got := &Response{}
+	err := got.Decode(b.Bytes())
+	require.NoError(t, err)
+	require.Equal(t, r, got)
 }
